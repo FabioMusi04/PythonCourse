30e937b73a6092a1ee9c83049248fbf0bc6abfa8
Fabio <fabio.musitelli04@gmail.com>
2024-09-19 16:50:34 +0200

    feat(order without lambda)

--- studentsOrder.py
+++ studentsOrder.py
@@ -41,10 +41,18 @@ print(studenti)
 def ordina_cognome_nome(studente):
     for s in studente:
         studente.sort(key=lambda x: (x["cognome"], x["nome"]))
+        
+        def sort_by_cognome_nome(x):
+            return x["cognome"], x["nome"]
+        studente.sort(key=sort_by_cognome_nome)
     return studente
 def ordina_nome_cognome(studente):
     for s in studente:
         studente.sort(key=lambda x: (x["nome"], x["cognome"]), reverse=True)
+        
+        def sort_by_nome_cognome(x):
+            return x["nome"], x["cognome"]
+        studente.sort(key=sort_by_nome_cognome, reverse=True)
     return studente
 print(ordina_cognome_nome(studenti))
 print(ordina_nome_cognome(studenti))
